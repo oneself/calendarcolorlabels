6b815a06876680a36c5a7740340c6ce06d15d752
Eyal Erez <eyal@spotify.com>
2021-05-29 18:04:27 +0300

    Adding Lavender color

--- shared.js
+++ shared.js
@@ -1,12 +1,13 @@
 const dotColors = {
+  "Tomato": "#D50000",
+  "Flamingo": "#E67C73",
+  "Tangerine": "#F4511E",
   "Banana": "#F6BF26",
   "Sage": "#33B679",
   "Basil": "#0B8043",
   "Peacock": "#039BE5",
-  "Graphite": "#616161",
-  "Flamingo": "#E67C73",
   "Blueberry": "#3F51B5",
-  "Tomato": "#D50000",
-  "Tangerine": "#F4511E",
-  "Grape": "#8E24AA"
+  "Lavender": "#7986CB",
+  "Grape": "#8E24AA",
+  "Graphite": "#616161"
 };
